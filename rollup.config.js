4377969ff0abf0f0cc2b1019be41dc6a929e19e5
mourrrya <mourrrya@gmail.com>
2024-05-08 17:25:25 +0530

    did some changes

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve  from "@rollup/plugin-node-resolve";
+import commonjs  from "@rollup/plugin-commonjs";
+import typescript  from "@rollup/plugin-typescript";
+import dts  from "rollup-plugin-dts";
+import packageJson  from "./package.json" with {type:"json"};
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "esm",
+      },
+    ],
+    plugins: [dts()],
+  },
+];
